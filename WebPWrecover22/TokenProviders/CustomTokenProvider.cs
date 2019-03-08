f634f1ab7d5431fc278cb7eaa05ee6a10a33c1e5
Ponant <19961806+Ponant@users.noreply.github.com>
2019-03-08 13:24:22 +0100

    Added custom email confirmation and custom token providers

---
+++ WebPWrecover22/TokenProviders/CustomTokenProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPWrecover.TokenProviders
+{
+    public class CustomEmailConformationTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomEmailConformationTokenProvider(IDataProtectionProvider dataProtectionProvider,
+            IOptions<EmailConfirmationTokenProviderOptions> options) : base(dataProtectionProvider, options)
+        {
+
+        }
+    }
+    public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+        public EmailConfirmationTokenProviderOptions()
+        {
+            Name = "EmailDataProtectorTokenProvider";
+            TokenLifespan = TimeSpan.FromSeconds(60);
+        }
+    }
+
+    public class CustomPasswordResetTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomPasswordResetTokenProvider(IDataProtectionProvider dataProtectionProvider,
+            IOptions<PasswordResetTokenProviderOptions> options) : base(dataProtectionProvider, options)
+        {
+
+        }
+    }
+    public class PasswordResetTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+        public PasswordResetTokenProviderOptions()
+        {
+            Name = "PasswordResetDataProtectorTokenProvider";
+            TokenLifespan = TimeSpan.FromSeconds(5);
+        }
+    }
+}
